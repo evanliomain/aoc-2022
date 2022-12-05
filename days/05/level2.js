2d0d151a8809410357e4861914aba43bff26c4e6
Evan Liomain <evan.liomain@gmail.com>
2022-12-05 13:47:01 +0100

    ✨ Solve day 5

---
+++ days/05/level2.js
@@ -0,0 +1,15 @@
+const T = require('taninsam');
+
+module.exports = function({ moves, stacks }) {
+  return T.chain(moves)
+    .chain(
+      T.reduce((s, { nb, from, to }) => {
+        const removed = s[from].splice(s[from].length - nb, nb);
+        s[to].push(...removed);
+        return s;
+      }, stacks)
+    )
+    .chain(T.map(T.last()))
+    .chain(T.join(''))
+    .value();
+};
