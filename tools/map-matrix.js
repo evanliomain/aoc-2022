52e8a1e383675429542ec7dd5959f73c4974e9c9
Evan Liomain <evan.liomain@gmail.com>
2022-12-08 10:06:42 +0100

    📝 Add documentation on mapMatrix tools function

--- tools/map-matrix.js
+++ tools/map-matrix.js
@@ -1,5 +1,10 @@
 const T = require('taninsam');
 
+/**
+ * Apply a function to each cell of a matrix
+ * @param {(cell: any, x: number, y: number, matrix: any[][])=>any} iteree the function to apply to each cell, take cell, x, y and matrix in arguments
+ * @returns a new matrix
+ */
 module.exports = function mapMatrix(iteree) {
   return matrix => {
     return T.chain(matrix)
